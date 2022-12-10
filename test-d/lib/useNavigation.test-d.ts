0daac58377f3951a06aeaba6183893a5e266d66c
JoseLion <joseluis5000l@gmail.com>
2022-12-10 15:28:57 -0500

    feat(useNavigation): Add navigation callbacks `goTo` and `resetTo`

--- test-d/lib/useNavigation.test-d.ts
+++ test-d/lib/useNavigation.test-d.ts
@@ -6,9 +6,17 @@ import { TestRoutes } from "../../test/helpers/routes";
 
 const { home, library } = TestRoutes;
 
-const { navigate, reset } = useNavigation();
+const { goTo, navigate, reset, resetTo } = useNavigation();
 
-expectType<Navigation>({ navigate, reset });
+expectType<Navigation>({ goTo, navigate, reset, resetTo });
+
+expectAssignable<(route: typeof home) => () => void>(goTo as typeof goTo<typeof home>);
+expectAssignable<(route: typeof library, params: { libId: number; }) => () => void>(
+  goTo as typeof goTo<typeof library>
+);
+expectAssignable<(route: typeof library.author, params: { authorId: number; libId: number; }) => () => void>(
+  goTo as typeof goTo<typeof library.author>
+);
 
 expectAssignable<(route: typeof home) => void>(navigate as typeof navigate<typeof home>);
 expectAssignable<(route: typeof library, params: { libId: number; }) => void>(
@@ -24,4 +32,12 @@ expectAssignable<(route: typeof library, params: { libId: number; }) => void>(
 );
 expectAssignable<(route: typeof library.author, params: { authorId: number; libId: number; }) => void>(
   reset as typeof reset<typeof library.author>
+);
+
+expectAssignable<(route: typeof home) => () => void>(resetTo as typeof resetTo<typeof home>);
+expectAssignable<(route: typeof library, params: { libId: number; }) => () => void>(
+  resetTo as typeof resetTo<typeof library>
+);
+expectAssignable<(route: typeof library.author, params: { authorId: number; libId: number; }) => () => void>(
+  resetTo as typeof resetTo<typeof library.author>
 );
