9b2580c6e9bc32ad72914398d51a7c60f2361a30
Jose Luis Leon <joseluis5000l@gmail.com>
2022-12-18 01:41:25 -0500

    feat(Routes): Support splats on routes (#17)

--- test/integration/lib/components/Routes.component.test.tsx
+++ test/integration/lib/components/Routes.component.test.tsx
@@ -1,44 +1,52 @@
 import { render, waitFor } from "@testing-library/react";
-import { BrowserRouter, Link } from "react-router-dom";
+import { BrowserRouter, Link as RRLink } from "react-router-dom";
 import userEvent from "@testing-library/user-event";
 
 import { Route } from "../../../../src/lib/components/Route.component";
 import { TestScreen } from "../../../helpers/renderWith";
 import { TestRoutes } from "../../../helpers/routes";
 import { Routes } from "../../../../src/lib/components/Routes.component";
+import { Link } from "../../../../src/lib/components/Link.component";
 
 const { home, library } = TestRoutes;
 
 describe("[Integration] Routes.component.test.tsx", () => {
   it("creates a Route and passing the template to the path", async () => {
-    window.history.pushState({ }, "", home.makeUrl());
+    window.history.pushState({ }, "", "/");
 
     const { getByRole, getByText } = render(
       <BrowserRouter>
         <nav>
           <h4>
-            <Link to={home.makeUrl()}>
+            <Link to={home}>
               {"Go Home"}
             </Link>
           </h4>
           <h4>
-            <Link to={library.makeUrl({ libId: 1 })}>
+            <RRLink to={`${home.makeUrl()}/plus/splat/path`}>
+              {"Go Home Splat"}
+            </RRLink>
+          </h4>
+          <h4>
+            <Link to={library} params={{ libId: 1 }}>
               {"Go Library"}
             </Link>
           </h4>
           <h4>
-            <Link to={library.author.makeUrl({ authorId: 1, libId: 1 })}>
+            <Link to={library.author} params={{ authorId: 1, libId: 1 }}>
               {"Go Author"}
             </Link>
           </h4>
           <h4>
-            <Link to={library.author.book.makeUrl({ authorId: 1, bookId: 1, libId: 1 })}>
+            <Link to={library.author.book} params={{ authorId: 1, bookId: 1, libId: 1 }}>
               {"Go Book"}
             </Link>
           </h4>
         </nav>
 
         <Routes>
+          <Route route="*" element={<TestScreen title="404" />} />
+          <Route route={home} catchAll={true} element={<TestScreen title="Home Splat" />} />
           <Route route={home} element={<TestScreen title="Home" />} />
           <Route route={library} element={<TestScreen title="Library" />} />
           <Route route={library.author} element={<TestScreen title="Author" />} />
@@ -47,7 +55,15 @@ describe("[Integration] Routes.component.test.tsx", () => {
       </BrowserRouter>
     );
 
+    await waitFor(() => getByRole("heading", { level: 1, name: "404" }));
+
+    await userEvent.click(getByText("Go Home"));
+
     await waitFor(() => getByRole("heading", { level: 1, name: "Home" }));
+
+    await userEvent.click(getByText("Go Home Splat"));
+
+    await waitFor(() => getByRole("heading", { level: 1, name: "Home Splat" }));
 
     await userEvent.click(getByText("Go Library"));
 
@@ -60,9 +76,5 @@ describe("[Integration] Routes.component.test.tsx", () => {
     await userEvent.click(getByText("Go Book"));
 
     await waitFor(() => getByRole("heading", { level: 1, name: "Book" }));
-
-    await userEvent.click(getByText("Go Home"));
-
-    await waitFor(() => getByRole("heading", { level: 1, name: "Home" }));
   });
 });
