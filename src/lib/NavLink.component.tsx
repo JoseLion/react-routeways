b11345556ac9d9d2a9b939d8597f32bfdd1d9337
JoseLion <joseluis5000l@gmail.com>
2022-12-17 23:00:42 -0500

    feat(linking): Add Link and NavLink replacement components

---
+++ src/lib/NavLink.component.tsx
@@ -0,0 +1,50 @@
+import { ReactElement, useMemo } from "react";
+import { CodecMap, PathLike, Routeway } from "ts-routeways";
+import { NavLink as OriginalNavLink, NavLinkProps as OriginalNavLinkProps } from "react-router-dom";
+
+interface BaseNavLinkProps<T extends Routeway> {
+  /**
+   * The {@link Routeway} route to navigate to.
+   */
+  to: T;
+}
+
+type ParamNavLinkProps<T extends Routeway> =
+  T extends Routeway<PathLike, infer V, CodecMap>
+    ? keyof V extends never
+      ? {
+        /**
+         * Optional query parameters to use when the path is generated.
+         */
+        params?: Parameters<T["makeUrl"]>[0];
+      }
+      : {
+        /**
+         * The path variables and optional query parameters to use when the
+         * path is generated
+         */
+        params: Parameters<T["makeUrl"]>[0];
+      }
+    : never;
+
+export type NavLinkProps<T extends Routeway> =
+  & BaseNavLinkProps<T>
+  & ParamNavLinkProps<T>
+  & Omit<OriginalNavLinkProps, "to">;
+
+/**
+ * A wrapper over react-router-dom's `<NavLink>` component, with the difference
+ * that the `to` prop expects a {@link Routeway} route instead of a path string.
+ *
+ * If the route requires path variables and/or query parameters, you can pass
+ * them over the `params` prop.
+ */
+export function NavLink<T extends Routeway>(props: NavLinkProps<T>): ReactElement {
+  const { to, params, ...rest } = props;
+
+  const url = useMemo((): string => {
+    return to.makeUrl(params);
+  }, [to, params]);
+
+  return <OriginalNavLink {...rest} to={url} />;
+}
