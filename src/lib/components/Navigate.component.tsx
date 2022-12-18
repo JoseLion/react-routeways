c9f0671037408e7a88c007dc6f394e4d84173d9d
JoseLion <joseluis5000l@gmail.com>
2022-12-18 01:09:05 -0500

    feat(navigate): Add Navigate component replacement

---
+++ src/lib/components/Navigate.component.tsx
@@ -0,0 +1,50 @@
+import { ReactElement, useMemo } from "react";
+import { CodecMap, PathLike, Routeway } from "ts-routeways";
+import { Navigate as OriginalNavigate, NavigateProps as OriginalNavigateProps } from "react-router-dom";
+
+interface BaseNavigateProps<T extends Routeway> {
+  /**
+   * The {@link Routeway} route to navigate to.
+   */
+  to: T;
+}
+
+type ParamNavigateProps<T extends Routeway> =
+  T extends Routeway<PathLike, infer V, CodecMap>
+    ? keyof V extends never
+      ? {
+        /**
+         * Optional query parameters to use when the path is generated.
+         */
+        params?: Parameters<T["makeUrl"]>[0];
+      }
+      : {
+        /**
+         * The path variables and optional query parameters to use when the
+         * path is generated
+         */
+        params: Parameters<T["makeUrl"]>[0];
+      }
+    : never;
+
+export type NavigateProps<T extends Routeway> =
+  & BaseNavigateProps<T>
+  & ParamNavigateProps<T>
+  & Omit<OriginalNavigateProps, "to">;
+
+/**
+ * A wrapper over react-router-dom's `<Navigate>` component, with the difference
+ * that the `to` prop expects a {@link Routeway} route instead of a path string.
+ *
+ * If the route requires path variables and/or query parameters, you can pass
+ * them over the `params` prop.
+ */
+export function Navigate<T extends Routeway>(props: NavigateProps<T>): ReactElement {
+  const { to, params, ...rest } = props;
+
+  const url = useMemo((): string => {
+    return to.makeUrl(params);
+  }, [to, params]);
+
+  return <OriginalNavigate {...rest} to={url} />;
+}
---
+++ test/types/lib/components/Navigate.component.typetest.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { expectTypeOf } from "expect-type";
+import { Routeway } from "ts-routeways";
+import { NavigateProps as OriginalnavigateProps } from "react-router-dom";
+
+import { TestRoutes } from "../../../helpers/routes";
+import { Navigate, NavigateProps } from "../../../../src/lib/components/Navigate.component";
+
+const { home, library } = TestRoutes;
+
+expectTypeOf<Omit<NavigateProps<Routeway>, "to">>().toMatchTypeOf<Omit<OriginalnavigateProps, "to">>();
+
+expectTypeOf(<Navigate to={home} />).toEqualTypeOf<JSX.Element>();
+expectTypeOf(<Navigate to={library} params={{ libId: 1, page: 1 }} />).toEqualTypeOf<JSX.Element>();
+expectTypeOf(<Navigate to={library.author} params={{ authorId: 1, libId: 1 }} />).toEqualTypeOf<JSX.Element>();
+expectTypeOf(
+  <Navigate
+    to={library.author.book}
+    params={{ authorId: 1, bookId: 1, libId: 1 }}
+  />
+)
+.toEqualTypeOf<JSX.Element>();
+
+// @ts-expect-error
+expectTypeOf(<Navigate to={library} />).toEqualTypeOf<JSX.Element>();
+// @ts-expect-error
+expectTypeOf(<Navigate to={library.author} params={{ libId: 1 }} />).toEqualTypeOf<JSX.Element>();
+// @ts-expect-error
+expectTypeOf(<Navigate to={library.author.book} params={{ authorId: 1, libId: 1 }} />).toEqualTypeOf<JSX.Element>();
